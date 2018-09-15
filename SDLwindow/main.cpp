e70400a0643d532caa65f8cfb2e8d41f5eaeede3
miguellopes91 <miguelpvz@hotmail.com>
2018-09-15 16:36:27 +0100

    Add game loop logic

---
+++ SDLwindow/main.cpp
@@ -0,0 +1,25 @@
+
+#include <SDL.h>
+#include <stdio.h>
+#include "Game.hpp"
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+Game *game = nullptr;
+
+int main(int argc, char *argv[])
+{
+	game = new Game();
+	game->init("GameWindow", SCREEN_WIDTH, SCREEN_HEIGHT, false);
+
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+	return 0;
+}
