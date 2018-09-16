3f49d8dcc62dec53901f69a59087e92278cbd7e9
miguellopes91 <miguelpvz@hotmail.com>
2018-09-16 18:37:53 +0100

    Fix ome compiling issues

---
+++ SDLwindow/PositionComponent.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Components.hpp"
+
+class PositionComponent : public Component
+{
+private:
+	int xpos;
+	int ypos;
+
+public:
+
+	PositionComponent() {
+		 xpos = 0;
+		 ypos = 0;
+	}
+	PositionComponent(int x, int y) {
+		xpos = x;
+		ypos = y;
+	}
+
+	//expose and set the y and x position
+	int x() { return xpos; }
+	int y() { return ypos; }
+
+	void init() override
+	{
+		xpos = 0;
+		ypos = 0;
+	}
+
+	void update() override
+	{
+		xpos++;
+		ypos++;
+	}
+
+
+	void setPos(int x, int y) {
+		xpos = x;
+		ypos = y;
+	}
+};
