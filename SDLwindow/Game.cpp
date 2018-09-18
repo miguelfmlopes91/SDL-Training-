ced89dda3a6401d4eca1a99e424dcb1ed854dac0
miguellopes91 <miguelpvz@hotmail.com>
2018-09-18 20:06:14 +0100

    Add tilemap support

--- SDLwindow/Game.cpp
+++ SDLwindow/Game.cpp
@@ -18,9 +18,6 @@ std::vector<ColliderComponent*> Game::colliders;
 auto& player(manager.addEntity());
 auto& wall(manager.addEntity());
 
-auto& tile0(manager.addEntity());
-auto& tile1(manager.addEntity());
-auto& tile2(manager.addEntity());
 
 
 Game::Game()
@@ -52,11 +49,7 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
 
 	map = new Map();
 
-	tile0.addComponent<TileComponent>(200, 200, 32, 32, 0);
-	tile1.addComponent<TileComponent>(250, 250, 32, 32, 1);
-	tile1.addComponent<ColliderComponent>("dirt");
-	tile2.addComponent<TileComponent>(150, 150, 32, 32, 2);
-	tile2.addComponent<ColliderComponent>("grass");
+	Map::LoadMap("Resources/mymap.map", 32, 32);
 
 
 	player.addComponent<TransformComponent>(2);
@@ -101,7 +94,7 @@ void Game::render()
 {
 	SDL_RenderClear(renderer);
 	//add all textures to be rendered
-	//map->DrawMap();
+
 
 	manager.draw();
 	SDL_RenderPresent(renderer);
@@ -112,4 +105,10 @@ void Game::clean()
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
+}
+
+void Game::AddTile(int id, int x, int y)
+{
+	auto& tile(manager.addEntity());
+	tile.addComponent<TileComponent>(x, y, 32, 32, id);
 }
--- SDLwindow/Map.hpp
+++ SDLwindow/Map.hpp
@@ -1,29 +1,15 @@
 #pragma once
 #include <string>
-#include "Game.hpp"
 
 class Map
 {
 public:
 	Map();
-	//Map(std::string tID, int ms, int ts);
 	~Map();
 
-	//void LoadMap(std::string path, int sizeX, int sizeY);
-	void LoadMap(int arr[20][25]);
-	void DrawMap();
-	//void AddTile(int srcX, int srcY, int xpos, int ypos);
+	static void LoadMap(std::string path,int sizeX, int sizeY);
+
 
 private:
-	//std::string texID;
-	//int mapScale;
-	//int tileSize;
-	//int scaledSize;
-	SDL_Rect src, dest;
 
-	SDL_Texture* dirt;
-	SDL_Texture* grass;
-	SDL_Texture* water;
-
-	int map[20][25];
 };
